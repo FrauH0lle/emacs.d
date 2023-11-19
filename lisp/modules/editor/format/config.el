a253517473ca3a0045b6245db7aea71d52bd3f89
FrauH0lle <roland.goers@posteo.de>
2023-11-19 08:13:43 +0100

    feat editor/format: format-all -> apheleia

--- lisp/modules/editor/format/config.el
+++ lisp/modules/editor/format/config.el
@@ -1,58 +1,74 @@
-;;; editor/format/config.el -*- lexical-binding: t; -*-
+;; editor/format/config.el -*- lexical-binding: t; -*-
+
+(defvar +format-on-save-disabled-modes
+  '(sql-mode           ; sqlformat is currently broken
+    tex-mode           ; latexindent is broken
+    latex-mode
+    org-msg-edit-mode) ; doesn't need a formatter
+  "A list of major modes in which to reformat the buffer upon saving.
+If this list begins with `not', then it negates the list.
+If it is `t', it is enabled in all modes.
+If nil, it is disabled in all modes, the same as if the +onsave flag wasn't
+  used at all.
+Irrelevant if you do not have the +onsave flag enabled for this module.")
 
 (defvar +format-preserve-indentation t
-  "If non-nil, the leading indentation is preserved when formatting
-the whole
+  "If non-nil, the leading indentation is preserved when formatting the whole
 buffer. This is particularly useful for partials.
 
 Indentation is always preserved when formatting regions.")
 
-(defvar-local +format-with nil
-  "Set this to explicitly use a certain formatter for the current
-buffer.")
-
 (defvar +format-with-lsp t
   "If non-nil, format with LSP formatter if it's available.
 
-This can be set buffer-locally with `setq-hook!' to disable LSP
-formatting in select buffers.")
+This can be set buffer-locally with `setq-hook!' to disable LSP formatting in
+select buffers.")
+
+(defvaralias '+format-with 'apheleia-formatter
+  "Set this to explicitly use a certain formatter for the current buffer.")
 
 
 ;;
 ;;; Bootstrap
 
-(add-to-list 'zenit-debug-variables 'format-all-debug)
+(when (modulep! +onsave)
+  (add-hook 'zenit-first-file-hook #'apheleia-global-mode))
+
+(defun +format-inhibit-maybe-h ()
+  "Enable formatting on save in certain major modes.
+This is controlled by `+format-on-save-disabled-modes'."
+  (or (eq major-mode 'fundamental-mode)
+      (string-blank-p (buffer-name))
+      (not (null (memq major-mode +format-on-save-disabled-modes)))))
+
+
+(after! apheleia-core
+  (add-to-list 'zenit-debug-variables '(apheleia-log-only-errors . nil))
+
+  (when (modulep! +onsave)
+    (add-to-list 'apheleia-inhibit-functions #'+format-inhibit-maybe-h)))
 
 
 ;;
 ;;; Hacks
 
-;; Allow a specific formatter to be used by setting `+format-with', either
-;; buffer-locally or let-bound.
-(advice-add #'format-all--probe :around #'+format-probe-a)
+(defadvice! +format--inhibit-reformat-on-prefix-arg-a (orig-fn &optional arg)
+  "Make it so \\[save-buffer] with prefix arg inhibits reformatting."
+  :around #'save-buffer
+  (let ((apheleia-mode (and apheleia-mode (memq arg '(nil 1)))))
+    (funcall orig-fn)))
 
-;; We use a modded `format-all-buffer', which
-;;   1. Enables partial reformatting (while preserving leading indentation),
-;;   2. Applies changes via RCS patch, line by line, to protect buffer markers
-;;      and avoid any jarring cursor+window scrolling.
-(advice-add #'format-all-buffer--with :override #'+format-buffer-a)
+(add-hook!
+ 'apheleia-post-format-hook
+ ;; HACK `web-mode' doesn't update syntax highlighting after arbitrary buffer
+ ;;      modifications, so we must trigger refontification manually.
+ (defun +format--fix-web-mode-fontification-h ()
+   (when (eq major-mode 'web-mode)
+     (setq web-mode-fontification-off nil)
+     (when (and web-mode-scan-beg web-mode-scan-end global-font-lock-mode)
+       (save-excursion
+         (font-lock-fontify-region web-mode-scan-beg web-mode-scan-end)))))
 
-;; format-all-mode "helpfully" raises an error when it doesn't know how to
-;; format a buffer.
-(add-to-list 'debug-ignored-errors "^Don't know how to format ")
-
-;; Don't pop up imposing warnings about missing formatters, but still log it in
-;; to *Messages*.
-(defadvice! +format--all-buffer-from-hook-a (fn &rest args)
-  :around #'format-all-buffer--from-hook
-  (letf! (defun format-all-buffer--with (formatter mode-result)
-           (when (or (eq formatter 'lsp)
-                     (eq formatter 'eglot)
-                     (condition-case-unless-debug e
-                         (format-all--formatter-executable formatter)
-                       (error
-                        (message "Warning: cannot reformat buffer because %S isn't installed"
-                                 (gethash formatter format-all--executable-table))
-                        nil)))
-             (funcall format-all-buffer--with formatter mode-result)))
-    (apply fn args)))
+ (defun +format--refresh-git-gutter-h ()
+   (when (fboundp '+vc-gutter-update-h)
+     (+vc-gutter-update-h))))

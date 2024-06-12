465e3db7b681672457c2c41970d0560470b63466
FrauH0lle <roland.goers@posteo.de>
2024-06-12 07:24:08 +0200

    tweak: Autoload macros in ui/popup

--- lisp/modules/ui/popup/config.el
+++ lisp/modules/ui/popup/config.el
@@ -125,41 +125,6 @@ disabled when that window has been changed or closed."
 
 
 ;;
-;;; Macros
-
-(defmacro with-popup-rules! (rules &rest body)
-  "Evaluate BODY with popup RULES. RULES is a list of popup rules.
-Each rule should match the arguments of `+popup-define' or the
-:popup setting."
-  (declare (indent defun))
-  `(let ((+popup--display-buffer-alist +popup--old-display-buffer-alist)
-         display-buffer-alist)
-     (set-popup-rules! ,rules)
-     (when (bound-and-true-p popper-mode)
-       (setq display-buffer-alist +popup--display-buffer-alist))
-     ,@body))
-
-(defmacro save-popups! (&rest body)
-  "Sets aside all popups before executing the original
-function,usually to prevent the popup(s) from messing up the
-UI (or vice versa)."
-  `(let* ((in-popup-p (popper-popup-p (current-buffer)))
-          (popups (+popup-windows))
-          (+popup--inhibit-transient t)
-          buffer-list-update-hook
-          +popup--last)
-     (dolist (p popups)
-       (+popup/close p 'force))
-     (unwind-protect
-         (progn ,@body)
-       (when popups
-         (let ((origin (selected-window)))
-           (+popup/restore)
-           (unless in-popup-p
-             (select-window origin)))))))
-
-
-;;
 ;;; Default popup rules & bootstrap
 
 (set-popup-rules!

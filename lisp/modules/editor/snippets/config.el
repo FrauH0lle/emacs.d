c46bbe0d2a55f84632365d5179c10640e4643f65
FrauH0lle <roland.goers@posteo.de>
2024-06-12 07:23:25 +0200

    tweak: editor/snippets
    - Move patches into use-package! blocks

--- lisp/modules/editor/snippets/config.el
+++ lisp/modules/editor/snippets/config.el
@@ -25,7 +25,7 @@ in `zenit-local-conf-dir' take precedence.")
   :defer t
   :config/el-patch
   ;; PATCH Allow specified minor modes to match the buffer local variable
-  ;; `+snippets--extra-modes'
+  ;;   `+snippets--extra-modes'
   (defun tempel--condition-p (modes plist)
   "Return non-nil if one of MODES matches and the PLIST condition is satisfied."
   (and
@@ -43,101 +43,11 @@ in `zenit-local-conf-dir' take precedence.")
          (save-restriction
            (save-match-data
              (eval (plist-get plist :when) 'lexical)))))))
-  :config
-  ;; Add snippet libraries
-  (setq tempel-path (zenit-files-in +snippets-dirs :match "\\.eld$"))
 
-  (defadvice! +tempel-update-tempel-path (&rest _)
-    "Updates `tempel-path'."
-    :before #'tempel-insert
-    :before #'tempel-complete
-    :before #'tempel-expand
-    (unless (bound-and-true-p +file-templates--expanding-p)
-      (setq tempel-path (zenit-files-in +snippets-dirs :match "\\.eld$"))))
-
-  ;; Register `def-project-mode!' modes with yasnippet. This enables project
-  ;; specific snippet libraries (e.g. for Laravel, React or Jekyll projects).
-  (add-hook 'zenit-project-hook #'+snippets-enable-project-modes-h)
-
-  ;; Exit snippets on ESC from normal mode
-  (add-hook 'zenit-escape-hook #'tempel-abort)
-
-  ;; REVIEW Maybe this is not needed
-  (after! smartparens
-    ;; Tell smartparens overlays not to interfere
-    (advice-add #'tempel-expand :before #'sp-remove-active-pair-overlay))
-
-  ;; Keybinds
-  (map! :map tempel-map
-        "<tab>"     #'tempel-next
-        "TAB"       #'tempel-next
-        "<backtab>" #'tempel-previous
-        "C-g"       #'tempel-abort
-        "C-<home>"  #'tempel-beginning
-        "C-<end>"   #'tempel-end))
-
-;; HACK This is rather hacky ...
-(use-package! lsp-snippet-tempel
-  :hook (lsp-mode . lsp-snippet-tempel-lsp-mode-init)
-  :init
-  (with-eval-after-load 'lsp-mode
-    ;; Fool `lsp-mode'
-    (defadvice! +snippets-fake-yas-minor-mode (fn &rest args)
-      :around #'lsp--client-capabilities
-      :around #'lsp-register-client
-      (if (fboundp 'yas-minor-mode)
-          (apply fn args)
-        (letf! ((#'yas-minor-mode #'ignore))
-          (apply fn args)))))
-  :config/el-patch
-  ;; PATCH `lsp-mode' now checks if `yas-minor-mode' is bound
-  (defun lsp-snippet-tempel-lsp-mode-init ()
-    (lsp-snippet-tempel--init)
-    (advice-add 'lsp--expand-snippet :override #'lsp-snippet-tempel--lsp-mode-expand-snippet)
-    ;; HACK `lsp-mode' enables snippet based on `(feature 'yasnippet)'
-    (el-patch-remove
-      (provide 'yasnippet))))
-
-(eval-when-compile
-  (require 'el-patch)
-  (require 'lsp-snippet))
-
-(el-patch-feature lsp-snippet)
-(with-eval-after-load 'lsp-snippet
-  (el-patch-defun lsp-snippet--escaped-string-to (stop-at)
-    (let ((start (lsp-snippet--index))
-          (continue t)
-          (el-patch-add
-            (result "")))
-      (while continue
-        ;; Skip next token if `backslash'
-        (when (lsp-snippet--take '(backslash))
-          ;; Skip next token
-          (lsp-snippet--ignore '(eof))
-          (el-patch-add
-            (setq result (concat result (substring lsp-snippet--str (1+ start) (lsp-snippet--index))))
-            (setq start (lsp-snippet--index))))
-        (setq continue (lsp-snippet--ignore
-                        `(eof . ,stop-at)))
-        (el-patch-add
-          (setq result (concat result (substring lsp-snippet--str start (lsp-snippet--index))))
-          (setq start (lsp-snippet--index))))
-      (el-patch-swap
-        (unless (eq start (lsp-snippet--index))
-          (substring lsp-snippet--str start (lsp-snippet--index)))
-        (when (length> result 0)
-          result)))))
-
-;; PATCH Ask user to choose if a LSP snippet offers choices. This is pretty
-;;   hacky and there should be a better way. See
-;;   https://github.com/minad/tempel/issues/105
-(eval-when-compile
-  (require 'el-patch)
-  (require 'tempel))
-
-(el-patch-feature tempel)
-(with-eval-after-load 'tempel
-  (el-patch-defun tempel--element (st region elt)
+  ;; PATCH Ask user to choose if a LSP snippet offers choices. This is pretty
+  ;;   hacky and there should be a better way. See
+  ;;   https://github.com/minad/tempel/issues/105
+  (defun tempel--element (st region elt)
     "Add template ELT to ST given the REGION."
     (pcase elt
       ('nil)
@@ -174,15 +84,93 @@ in `zenit-local-conf-dir' take precedence.")
       (_ (if-let ((ret (run-hook-with-args-until-success 'tempel-user-elements elt)))
              (tempel--element st region ret)
            ;; TEMPEL EXTENSION: Evaluate forms
-           (tempel--form st elt))))))
+           (tempel--form st elt)))))
+  :config
+  ;; Add snippet libraries
+  (setq tempel-path (zenit-files-in +snippets-dirs :match "\\.eld$"))
 
-(eval-when-compile
-  (require 'el-patch)
-  (require 'lsp-snippet-tempel))
+  (defadvice! +tempel-update-tempel-path (&rest _)
+    "Updates `tempel-path'."
+    :before #'tempel-insert
+    :before #'tempel-complete
+    :before #'tempel-expand
+    (unless (bound-and-true-p +file-templates--expanding-p)
+      (setq tempel-path (zenit-files-in +snippets-dirs :match "\\.eld$"))))
 
-(el-patch-feature lsp-snippet-tempel)
-(with-eval-after-load 'lsp-snippet-tempel
-  (el-patch-defun lsp-snippet-tempel--choice-fn (number choices)
+  ;; Register `def-project-mode!' modes with yasnippet. This enables project
+  ;; specific snippet libraries (e.g. for Laravel, React or Jekyll projects).
+  (add-hook 'zenit-project-hook #'+snippets-enable-project-modes-h)
+
+  ;; Exit snippets on ESC from normal mode
+  (add-hook 'zenit-escape-hook #'tempel-abort)
+
+  ;; REVIEW Maybe this is not needed
+  (after! smartparens
+    ;; Tell smartparens overlays not to interfere
+    (advice-add #'tempel-expand :before #'sp-remove-active-pair-overlay))
+
+  ;; Keybinds
+  (map! :map tempel-map
+        "<tab>"     #'tempel-next
+        "TAB"       #'tempel-next
+        "<backtab>" #'tempel-previous
+        "C-g"       #'tempel-abort
+        "C-<home>"  #'tempel-beginning
+        "C-<end>"   #'tempel-end))
+
+;; HACK This is rather hacky ...
+(use-package! lsp-snippet
+  :defer t
+  :config/el-patch
+  ;; PATCH Clumsy approach to fix
+  ;;   https://github.com/svaante/lsp-snippet/issues/5
+  (defun lsp-snippet--escaped-string-to (stop-at)
+    (let ((start (lsp-snippet--index))
+          (continue t)
+          (el-patch-add
+            (result "")))
+      (while continue
+        ;; Skip next token if `backslash'
+        (when (lsp-snippet--take '(backslash))
+          ;; Skip next token
+          (lsp-snippet--ignore '(eof))
+          (el-patch-add
+            (setq result (concat result (substring lsp-snippet--str (1+ start) (lsp-snippet--index))))
+            (setq start (lsp-snippet--index))))
+        (setq continue (lsp-snippet--ignore
+                        `(eof . ,stop-at)))
+        (el-patch-add
+          (setq result (concat result (substring lsp-snippet--str start (lsp-snippet--index))))
+          (setq start (lsp-snippet--index))))
+      (el-patch-swap
+        (unless (eq start (lsp-snippet--index))
+          (substring lsp-snippet--str start (lsp-snippet--index)))
+        (when (length> result 0)
+          result)))))
+
+(use-package! lsp-snippet-tempel
+  :hook (lsp-mode . lsp-snippet-tempel-lsp-mode-init)
+  :init
+  (with-eval-after-load 'lsp-mode
+    ;; Fool `lsp-mode'
+    (defadvice! +snippets-fake-yas-minor-mode (fn &rest args)
+      :around #'lsp--client-capabilities
+      :around #'lsp-register-client
+      (if (fboundp 'yas-minor-mode)
+          (apply fn args)
+        (letf! ((#'yas-minor-mode #'ignore))
+          (apply fn args)))))
+  :config/el-patch
+  ;; PATCH `lsp-mode' now checks if `yas-minor-mode' is bound
+  (defun lsp-snippet-tempel-lsp-mode-init ()
+    (lsp-snippet-tempel--init)
+    (advice-add 'lsp--expand-snippet :override #'lsp-snippet-tempel--lsp-mode-expand-snippet)
+    ;; HACK `lsp-mode' enables snippet based on `(feature 'yasnippet)'
+    (el-patch-remove
+      (provide 'yasnippet)))
+
+  ;; PATCH Return custom \\='lsp-choice element for `tempel--element'
+  (defun lsp-snippet-tempel--choice-fn (number choices)
     (el-patch-swap
       (lsp-snippet-tempel--placeholder-fn number (string-join choices ","))
       (let ((sym (intern (format "tabstop-%d" number))))

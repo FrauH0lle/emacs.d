d288322d9f793389928671c6cf19fa14c8b04922
FrauH0lle <roland.goers@posteo.de>
2024-06-15 15:43:11 +0200

    feat: Add emacs/eshell module

---
+++ lisp/modules/emacs/eshell/packages.el
@@ -0,0 +1,13 @@
+;; -*- lexical-binding: t; no-byte-compile: t; -*-
+;; emacs/eshell/packages.el
+
+(package! eshell-up :lockfile emacs-eshell)
+(package! eshell-z :lockfile emacs-eshell)
+(package! shrink-path :lockfile emacs-eshell)
+(package! esh-help :lockfile emacs-eshell)
+(package! eshell-did-you-mean :lockfile emacs-eshell)
+(package! eshell-syntax-highlighting :lockfile emacs-eshell)
+
+(unless IS-WINDOWS
+  (package! fish-completion :lockfile emacs-eshell)
+  (package! bash-completion :lockfile emacs-eshell))

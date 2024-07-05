01775c1b258ead3cbb9bd7c5ba9a3b6bdd92f444
FrauH0lle <roland.goers@posteo.de>
2024-07-05 10:27:42 +0200

    pkg: Update tree-sitter packages

--- straight/versions/tools-tree-sitter.el
+++ straight/versions/tools-tree-sitter.el
@@ -1,4 +1,4 @@
-(("elisp-tree-sitter" . "3cfab8a0e945db9b3df84437f27945746a43cc71")
+(("elisp-tree-sitter" . "02fe7b86d92b1aab954045146469b7893f0ab371")
  ("evil-textobj-tree-sitter" . "a19ab9d89a00f4a04420f9b5d61b66f04fea5261")
- ("tree-sitter-langs" . "acc7a3bdf1cb8762de4002251294aad83d0fef6f"))
+ ("tree-sitter-langs" . "99dd63d41be0d7d55d98fb7e8a23b569eebadbdc"))
 :gamma

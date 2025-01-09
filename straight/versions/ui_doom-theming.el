a29d633947ec98f47ae8c3ea2508aa89250d4904
FrauH0lle <roland.goers@posteo.de>
2025-01-09 22:37:20 +0100

    up

--- straight/versions/ui_doom-theming.el
+++ straight/versions/ui_doom-theming.el
@@ -1,3 +1,3 @@
-(("emacs-solaire-mode" . "8ccdceeb9298b3c4e35f630914f467bf164f39ad")
- ("themes" . "3b2422b208d28e8734b300cd3cc6a7f4af5eba55"))
+(("emacs-solaire-mode" . "c9334666bd208f3322e6118d30eba1b2438e2bb9")
+ ("themes" . "3c03f525d5c0ac0859f31231778f97e10a705e0d"))
 :gamma

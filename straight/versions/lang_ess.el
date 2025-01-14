c8909b02c5ad4ecd6c46c17c04e82117cadbc644
FrauH0lle <roland.goers@posteo.de>
2025-01-14 16:24:36 +0100

    pkg: Update ESS

--- straight/versions/lang_ess.el
+++ straight/versions/lang_ess.el
@@ -1,6 +1,6 @@
-(("ESS" . "e4f2afb90d11613c4c2a28720dcda226699b4dfb")
+(("ESS" . "0eb240bcb6d0e933615f6cfaa9761b629ddbabdd")
  ("ess-R-data-view.el" . "d6e98d3ae1e2a2ea39a56eebcdb73e99d29562e9")
  ("poly-R" . "8024e852cfca642dea2045a41b2033baa2f1f9a5")
- ("polymode" . "ca060e081a1f849a880732670dc15370ac987b89")
+ ("polymode" . "74ba75d4bcfbea959ccc9080a95ab9ef759849f2")
  ("xterm-color" . "2ad407c651e90fff2ea85d17bf074cee2c022912"))
 :gamma

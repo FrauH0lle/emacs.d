d90ccfb16d9b15f43e80b1ee464765895f7e4f7b
FrauH0lle <roland.goers@posteo.de>
2025-03-24 18:14:26 +0100

    fix(snippets): Update lsp-snippet patches
    * lisp/modules/editor/snippets/patches/lsp-snippet-tempel.el (lsp-snippet-tempel-lsp-mode-init):
      Ensure patch is based on latest function version.

--- lisp/modules/editor/snippets/patches/lsp-snippet-tempel.el
+++ lisp/modules/editor/snippets/patches/lsp-snippet-tempel.el
@@ -9,11 +9,14 @@
 
 ;; PATCH `lsp-mode' now checks if `yas-minor-mode' is bound
 (el-patch-defun lsp-snippet-tempel-lsp-mode-init ()
+  "Use `tempel' as `lsp-mode's snippet engine."
   (lsp-snippet-tempel--init)
   (advice-add 'lsp--expand-snippet :override #'lsp-snippet-tempel--lsp-mode-expand-snippet)
-  ;; HACK `lsp-mode' enables snippet based on `(feature 'yasnippet)'
+  ;; HACK `lsp-mode' enables snippet based on (fboundp 'yas-minor-mode)
   (el-patch-remove
-    (provide 'yasnippet)))
+    (unless (fboundp 'yas-minor-mode)
+      (defun yas-minor-mode (&rest _)
+        (error "Stub created by `lsp-snippet-tempel-lsp-mode-init'")))))
 
 ;; PATCH Return custom 'lsp-choice element for `tempel--element'
 (el-patch-defun lsp-snippet-tempel--choice-fn (number choices)

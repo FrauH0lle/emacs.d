d15809290996a57140cde3ac04e23944f2b1a0e2
FrauH0lle <roland.goers@posteo.de>
2025-04-06 20:13:17 +0200

    tweak(ui): tooltip-resize-echo-area = t
    * lisp/core/zenit-ui.el: Set tooltip-resize-echo-area to t.

--- lisp/core/zenit-ui.el
+++ lisp/core/zenit-ui.el
@@ -430,7 +430,8 @@ buffers are visible in other windows, switch to
 
 ;; Expand the minibuffer to fit multi-line text displayed in the echo-area. This
 ;; doesn't look too great with direnv, however...
-(setq resize-mini-windows 'grow-only)
+(setq resize-mini-windows 'grow-only
+      tooltip-resize-echo-area t)
 
 ;; Typing yes/no is obnoxious when y/n will do
 (if (boundp 'use-short-answers)

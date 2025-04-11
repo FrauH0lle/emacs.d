5197dea4a861b0fba328ebb30c2b78bae89a4ca8
FrauH0lle <roland.goers@posteo.de>
2025-04-11 09:03:13 +0200

    fix(format): lsp formatters
    * lisp/modules/editor/format/autoload/lsp.el: Ensure formatter
      forwarding is working as expected.
    
    * lisp/modules/editor/format/autoload/format.el (+format-region): Set
      region bounds during formatting.

--- lisp/modules/editor/format/autoload/format.el
+++ lisp/modules/editor/format/autoload/format.el
@@ -1,6 +1,5 @@
 ;; editor/format/autoload.el -*- lexical-binding: t; -*-
 
-
 (defun +format--current-indentation ()
   (save-excursion
     (goto-char (point-min))
@@ -8,6 +7,9 @@
     (current-indentation)))
 
 ;;;###autoload (autoload 'apheleia--get-formatters "apheleia-formatters")
+
+;;;###autoload
+(defvar +format--region-p nil)
 
 ;;;###autoload
 (defun +format-region (start end &optional callback)
@@ -42,22 +44,21 @@
           (when (> indent 0)
             (indent-rigidly (point-min) (point-max) (- indent)))
           ;;
-          (apheleia-format-buffer
-           command
-           (lambda ()
-             (with-current-buffer formatted-buffer
-               (when (> indent 0)
-                 ;; restore indentation without affecting new indentation
-                 (indent-rigidly (point-min) (point-max)
-                                 (max 0 (- indent (+format--current-indentation)))))
-               (set-buffer-modified-p nil))
-             (with-current-buffer cur-buffer
-               (delete-region start end)
-               (goto-char start)
-               (save-excursion
-                 (insert-buffer-substring-no-properties formatted-buffer)
-                 (when callback (funcall callback)))
-               (kill-buffer formatted-buffer)))))
+          (let ((+format--region-p (cons start end)))
+            (apheleia-format-buffer
+             command
+             (lambda ()
+               (with-current-buffer formatted-buffer
+                 (when (> indent 0)
+                   ;; Restore indentation without affecting new indentation
+                   (indent-rigidly (point-min) (point-max)
+                                   (max 0 (- indent (+format--current-indentation)))))
+                 (set-buffer-modified-p nil))
+               (with-current-buffer cur-buffer
+                 (with-silent-modifications
+                   (replace-region-contents start end (lambda () formatted-buffer) 5))
+                 (when callback (funcall callback))
+                 (kill-buffer formatted-buffer))))))
       (when (zenit-region-active-p)
         (setq deactivate-mark t)))))
 
@@ -69,20 +70,26 @@
 (defalias '+format/buffer #'apheleia-format-buffer)
 
 ;;;###autoload
-(defun +format/region (beg end &optional _arg)
+(defun +format/region (beg end &optional _arg interactive)
   "Format the selected region.
 
 WARNING: if the formatter doesn't support partial formatting,
 this command tries to pretend the active selection is the
 contents of a standalone file, but this may not always work. Keep
 your undo keybind handy!"
-  (interactive "rP")
-  (+format-region beg end))
+  (interactive (list (zenit-region-beginning)
+                     (zenit-region-end)
+                     current-prefix-arg
+                     'interactive))
+  (+format-region
+   beg end
+   (lambda ()
+     (when interactive
+       (message "Region reformatted!")))))
 
 ;;;###autoload
 (defun +format/region-or-buffer ()
-  "Format the selected region, or whole buffer if nothing is
-selected."
+  "Format the selected region, or whole buffer if nothing is selected."
   (interactive)
   (call-interactively
    (if (zenit-region-active-p)
--- lisp/modules/editor/format/autoload/lsp.el
+++ lisp/modules/editor/format/autoload/lsp.el
@@ -5,8 +5,7 @@
 
 ;;;###autoload
 (define-minor-mode +format-with-lsp-mode
-  "Toggles `lsp-mode'/`eglot' integration with `apheleia-mode' in
- this buffer.
+  "Toggles `lsp-mode'/`eglot' integration with `apheleia-mode' in this buffer.
 
 When enabled, it changes `+format-with' to `lsp', and back to its
 old value when disabled. Use `+format-wtih-lsp-maybe-h' to
@@ -25,8 +24,7 @@ with pre-existing user config on `+format-with'."
 (defvar +format--lsp-alist
   '((lsp-managed-mode . +format--with-lsp-mode)
     (eglot--managed-mode . +format--with-eglot))
-  "A list of LSP formatter functions to try on
- `+format-lsp-buffer'.")
+  "A list of LSP formatter functions to try on `+format-lsp-buffer'.")
 
 (defun +format--lsp-fn ()
   (cl-loop for (sym . fn) in +format--lsp-alist
@@ -35,8 +33,7 @@ with pre-existing user config on `+format-with'."
 
 ;;;###autoload
 (defun +format-with-lsp-toggle-h ()
-  "Toggle `+format-with-lsp-mode' depending on the state of
- lsp-mode/eglot.
+  "Toggle `+format-with-lsp-mode' depending on the state of lsp-mode/eglot.
 
 Does not activate the mode if `+format-with' is already set. To
 activate the mode unconditionally, call `+format-with-lsp-mode'
@@ -50,42 +47,38 @@ instead."
 
 ;;;###autoload
 (cl-defun +format-lsp-buffer (&rest plist &key buffer callback &allow-other-keys)
-  "Format the current buffer with any available lsp-mode or eglot
- formatter."
+  "Format the current buffer with any available lsp-mode or eglot formatter."
   (if-let* ((fn (with-current-buffer buffer (+format--lsp-fn)))
-            ((apply fn plist)))
+            ((apply fn (car +format--region-p) (cdr +format--region-p)
+                    plist)))
       (funcall callback)
     (funcall callback "LSP server doesn't support formatting")))
 
-(cl-defun +format--with-lsp-mode (&key buffer scratch &allow-other-keys)
-  "Format the current buffer with any available lsp-mode
- formatter."
+(cl-defun +format--with-lsp-mode (beg end &key buffer callback &allow-other-keys)
+  "Format the current buffer or region with any available lsp-mode formatter.
+
+Won't forward the buffer to chained formatters if successful."
   (with-current-buffer buffer
     (let ((edits
-           (cond
-            ((lsp-feature? "textDocument/formatting")
-             (lsp-request "textDocument/formatting"
-                          (lsp--make-document-formatting-params)))
-            ((lsp-feature? "textDocument/rangeFormatting")
-             (lsp-request "textDocument/rangeFormatting"
-                          (lsp--make-document-range-formatting-params
-                           (point-min) (point-max))))
-            (:err))))
-      (unless (eq edits :err)
-        (unless (seq-empty-p edits)
-          (with-current-buffer scratch
-            (lsp--apply-text-edits edits 'format)))
-        t))))
-
-(cl-defun +format--with-eglot (&key buffer scratch &allow-other-keys)
-  "Format the current buffer with any available eglot formatter."
-  (with-current-buffer scratch
-    (when (setq-local
-           eglot--cached-server
-           (with-current-buffer buffer
-             (when (or (eglot-server-capable :documentFormattingProvider)
-                       (eglot-server-capable :documentRangeFormattingProvider))
-               (eglot-current-server))))
-      (let ((buffer-file-name (buffer-file-name buffer)))
-        (eglot-format-buffer))
+           (cond ((and (null beg) (lsp-feature? "textDocument/formatting"))
+                  (lsp-request "textDocument/formatting"
+                               (lsp--make-document-formatting-params)))
+                 ((lsp-feature? "textDocument/rangeFormatting")
+                  (lsp-request "textDocument/rangeFormatting"
+                               (lsp--make-document-range-formatting-params
+                                (or beg (point-min)) (or end (point-max)))))
+                 ;; try next chained formatter(s)
+                 ((cl-return (ignore (funcall callback)))))))
+      (unless (seq-empty-p edits)
+        (lsp--apply-text-edits edits 'format))
       t)))
+
+(cl-defun +format--with-eglot (beg end &key buffer callback &allow-other-keys)
+  "Format the current buffer or region with any available eglot formatter.
+
+Won't forward the buffer to chained formatters if successful."
+  (with-current-buffer buffer
+    (or (with-demoted-errors "%s"
+          (always (eglot-format beg end)))
+        ;; try next chained formatter(s)
+        (ignore (funcall callback)))))

9e6ede5a0f51a1ccd63f0af160c9ec47b6503f58
FrauH0lle <roland.goers@posteo.de>
2025-04-27 20:41:46 +0200

    pkg: Bump packages
    * straight/versions/core.el: New repository pins.
    
    * straight/versions/emacs_org_pretty.el: Drop `org-fancy-priorities` and
      `org-superstar-mode`. Add `org-modern`.
    
    * straight/versions/lang_ess.el: Drop `ess-R-data-view.el`, add
      `ess-view-data` and `quarto-emacs`.

--- straight/versions/emacs_org_pretty.el
+++ straight/versions/emacs_org_pretty.el
@@ -1,4 +1,3 @@
 (("org-appear" . "32ee50f8fdfa449bbc235617549c1bccb503cb09")
- ("org-fancy-priorities" . "7f677c6c14ecf05eab8e0efbfe7f1b00ae68eb1d")
- ("org-superstar-mode" . "54c81c27dde2a6dc461bb064e79a8b2089093a2e"))
+ ("org-modern" . "3cc432dc99f262579d1cc464e7d6d5b9fe77083a"))
 :gamma
--- straight/versions/lang_ess.el
+++ straight/versions/lang_ess.el
@@ -1,6 +1,7 @@
 (("ESS" . "0eb240bcb6d0e933615f6cfaa9761b629ddbabdd")
- ("ess-R-data-view.el" . "d6e98d3ae1e2a2ea39a56eebcdb73e99d29562e9")
+ ("ess-view-data" . "dd6a85935bbee0f497d0e8731abdfa07150600b7")
  ("poly-R" . "8024e852cfca642dea2045a41b2033baa2f1f9a5")
  ("polymode" . "74ba75d4bcfbea959ccc9080a95ab9ef759849f2")
+ ("quarto-emacs" . "a7b974f7d22ef939eaed8b9919434bcf20b1438f")
  ("xterm-color" . "2ad407c651e90fff2ea85d17bf074cee2c022912"))
 :gamma

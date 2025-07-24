d60f943cfabd7a04ba3177b321044b04e35a15e4
FrauH0lle <roland.goers@posteo.de>
2025-07-24 11:29:48 +0200

    chore: Update package versions for core and LLM tools
    * straight/versions/core.el: New pins for package repositories.
    
    * straight/versions/tools_llm.el: New pins for packages.

--- straight/versions/tools_llm.el
+++ straight/versions/tools_llm.el
@@ -1,4 +1,5 @@
-(("gptel-quick" . "495b5e0b5348dbced1448bd12cbf8847e30b5175")
- ("aidermacs" . "02f8f3ea6ab6c5b7c554c81dd4eb729762ad6910")
- ("gptel" . "a5af15c770b66a61d0609a736e1db37495655559"))
+(("macher" . "377b913cd838f4feeeb0082827f0f5bc723dec03")
+ ("gptel-quick" . "495b5e0b5348dbced1448bd12cbf8847e30b5175")
+ ("aidermacs" . "fcdbd6c5845c0dfe5a7ddd753598e26e8c508bd3")
+ ("gptel" . "fc78b45a78b417c76b32b738f54664b4fe87c220"))
 :gamma

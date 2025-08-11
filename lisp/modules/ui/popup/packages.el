fd350ec700929ba16d89e95e91f8c027a585b103
FrauH0lle <roland.goers@posteo.de>
2025-08-11 07:33:06 +0200

    fix(popup): Remove leftover package
    `popper` is not used by the module. Instead it uses `popper`'s best
    ideas.
    
    * lisp/modules/ui/popup/packages.el: Deleted file.
    
    * straight/versions/ui_popup.el: Ditto.

--- lisp/modules/ui/popup/packages.el
+++
@@ -1,4 +0,0 @@
-;; -*- lexical-binding: t; no-byte-compile: t; -*-
-;; ui/popup/packages.el
-
-(package! popper :lockfile ui_popup)
--- straight/versions/ui_popup.el
+++
@@ -1,2 +0,0 @@
-(("popper" . "49f4904480cf4ca5c6db83fcfa9e6ea8d4567d96"))
-:gamma

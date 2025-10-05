b320db3ad155a251ddc23228783e94dafee2b159
FrauH0lle <roland.goers@posteo.de>
2025-10-05 06:56:56 +0200

    feat: Add :emacs ibuffer
    Add `ibuffer` module.
    
    * lisp/modules/emacs/ibuffer/README.org: Add file.
    
    * lisp/modules/emacs/ibuffer/autoload/workspaces.el: Ditto.
    
    * lisp/modules/emacs/ibuffer/config.el: Ditto.
    
    * lisp/modules/emacs/ibuffer/packages.el: Ditto.

---
+++ lisp/modules/emacs/ibuffer/packages.el
@@ -0,0 +1,5 @@
+;; -*- lexical-binding: t; no-byte-compile: t; -*-
+;; emacs/ibuffer/packages.el
+
+(package! ibuffer-projectile :lockfile emacs_ibuffer)
+(package! ibuffer-vc :lockfile emacs_ibuffer)

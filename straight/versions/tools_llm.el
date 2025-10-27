f8665a01a781d1d1f661e5aee4e6179be63a521f
FrauH0lle <roland.goers@posteo.de>
2025-10-27 16:33:37 +0100

    chore: Update packages
    * straight/versions/core.el:
    
    * straight/versions/tools_llm.el:

--- straight/versions/tools_llm.el
+++ straight/versions/tools_llm.el
@@ -1,6 +1,6 @@
-(("mevedel" . "d2e1aae78c10e2f6cc3981b6691a97fea294c8df")
+(("mevedel" . "1b17231052da7b08340ff719c1e35e6e535d7790")
  ("macher" . "4fa8fbb6b250b207723d380931a463bcbc8da9ca")
  ("gptel-quick" . "495b5e0b5348dbced1448bd12cbf8847e30b5175")
- ("gptel" . "29b731784184440efb51e7aee6eff060e88ee08b")
- ("mcp.el" . "b9782bb80027c13a84ae3ffff1e363a674ed6cf1"))
+ ("gptel" . "5723491daae983ea978c64c2dbe774238b58e3fd")
+ ("mcp.el" . "d1a4350815d13f1a351635c335be51a4ce6e0751"))
 :epsilon

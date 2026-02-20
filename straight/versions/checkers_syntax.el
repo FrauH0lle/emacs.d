1d0a77df90805b01b6ae0c6638de38ce2f81d9e6
FrauH0lle <roland.goers@posteo.de>
2026-02-20 19:41:41 +0100

    chore(syntax): Bump packages.
    * straight/versions/checkers_syntax.el: Update packages.

--- straight/versions/checkers_syntax.el
+++ straight/versions/checkers_syntax.el
@@ -1,4 +1,4 @@
-(("flycheck" . "8c0dbfaa463e69efb41ef2cda9c4e89b99e266a0")
- ("flycheck-popup-tip" . "f127be7058363056bb25c34b5831f45a7a232c12")
+(("flycheck" . "ebddfd89b1eea91b8590f542908672569942fb82")
+ ("flycheck-popup-tip" . "ef86aad907f27ca076859d8d9416f4f7727619c6")
  ("flymake-popon" . "99ea813346f3edef7220d8f4faeed2ec69af6060"))
 :epsilon

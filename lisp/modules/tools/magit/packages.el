7e359df7141e0e146777c5c09b1439ec788d0263
FrauH0lle <roland.goers@posteo.de>
2026-03-01 10:14:52 +0100

    chore(magit): Pin transient and cond-let
    Otherwise this breaks surprisingly often.
    
    * lisp/modules/tools/magit/packages.el: Pin packages.
    
    * straight/versions/tools_magit.el: Ditto.

--- lisp/modules/tools/magit/packages.el
+++ lisp/modules/tools/magit/packages.el
@@ -1,12 +1,16 @@
 ;; -*- no-byte-compile: t; -*-
 ;; tools/magit/packages.el
 
-(when (package! magit :lockfile tools_magit)
-  (package! magit-todos :lockfile tools_magit)
-  (when (modulep! +forge)
-    (package! forge :lockfile tools_magit)
-    (package! code-review
-      :recipe (:host github
-               :repo "doomelpa/code-review"
-               :files ("graphql" "code-review*.el"))
-      :lockfile tools_magit)))
+(package! transient :lockfile tools_magit)
+(package! cond-let :lockfile tools_magit)
+
+(package! magit :lockfile tools_magit)
+(package! magit-todos :lockfile tools_magit)
+
+(when (modulep! +forge)
+  (package! forge :lockfile tools_magit)
+  (package! code-review
+    :recipe (:host github
+             :repo "doomelpa/code-review"
+             :files ("graphql" "code-review*.el"))
+    :lockfile tools_magit))
--- straight/versions/tools_magit.el
+++ straight/versions/tools_magit.el
@@ -1,5 +1,7 @@
 (("code-review" . "303edcfbad8190eccb9a9269dfc58ed26d386ba5")
  ("forge" . "bbecd8947a190894570b8344490a50ebf0efd394")
- ("magit" . "dc0094bd88a5307fdfa1c2a48f3ec5b33891f1f0")
+ ("magit" . "c800f79c2061621fde847f6a53129eca0e8da728")
+ ("transient" . "1f7039ef8d548d6fe858084fcbeae7588eba4190")
+ ("cond-let" . "8bf87d45e169ebc091103b2aae325aece3aa804d")
  ("magit-todos" . "bd27c57eada0fda1cc0a813db04731a9bcc51b7b"))
 :epsilon

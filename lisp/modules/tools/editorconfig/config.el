b36e0fe555fd7d0b484111dce9f0f3453a3f42ba
FrauH0lle <roland.goers@posteo.de>
2026-03-16 09:46:05 +0100

    refactor(editorconfig): Use builtin package
    * lisp/modules/tools/editorconfig/config.el: Adjust config to internal
    package.
    
    * lisp/modules/tools/editorconfig/packages.el: Delete file.

--- lisp/modules/tools/editorconfig/config.el
+++ lisp/modules/tools/editorconfig/config.el
@@ -1,22 +1,10 @@
 ;; tools/editorconfig/config.el -*- lexical-binding: t; -*-
 
-;; REVIEW 2025-09-12: Revisit the module in Emacs v31. `editorconfig' is part of
-;;  Emacs v30 but it behaved rather different and caused some issues not worth
-;;  investigating.
 (use-package! editorconfig
   :hook (zenit-first-buffer . editorconfig-mode)
   :config
-  ;; The elisp implementation is the default, rather than the external,
-  ;; editorconfig binary, because upstream claims it's "faster and more secure".
-  (setq editorconfig-get-properties-function #'editorconfig-get-properties)
-
   (when (require 'ws-butler nil t)
     (setq editorconfig-trim-whitespaces-mode 'ws-butler-mode))
-
-  ;; Archives don't need editorconfig settings, and they may otherwise interfere
-  ;; with the process of opening them (office formats are zipped XML formats).
-  (add-to-list 'editorconfig-exclude-regexps
-               "\\.\\(zip\\|\\(doc\\|xls\\|ppt\\)x\\)\\'")
 
   (add-hook! 'editorconfig-after-apply-functions
     (defun +editorconfig-disable-indent-detection-h (props)
--- lisp/modules/tools/editorconfig/packages.el
+++
@@ -1,6 +0,0 @@
-;; -*- lexical-binding: t; no-byte-compile: t; -*-
-;; tools/editorconfig/packages.el
-
-(package! editorconfig
-  :recipe (:nonrecursive t)
-  :lockfile tools_editorconfig)

f6fefe50d3450eeacad3cd079c8d25822d94b680
FrauH0lle <roland.goers@posteo.de>
2026-03-16 09:49:14 +0100

    refactor: Rename doom-dashboard to dashboard

--- lisp/modules/ui/dashboard/autoload.el
+++ lisp/modules/ui/dashboard/autoload.el
@@ -1,28 +1,28 @@
-;; ui/doom-dashboard/autoload.el -*- lexical-binding: t; -*-
+;; ui/dashboard/autoload.el -*- lexical-binding: t; -*-
 
-(defun +doom-dashboard--help-echo ()
+(defun +dashboard--help-echo ()
   (when-let* ((btn (button-at (point)))
               (msg (button-get btn 'help-echo)))
     (message "%s" msg)))
 
 ;;;###autoload
-(defun +doom-dashboard/open (frame)
+(defun +dashboard/open (frame)
   "Switch to the dashboard in the current window, of the current FRAME."
   (interactive (list (selected-frame)))
   (with-selected-frame frame
     (switch-to-buffer (zenit-fallback-buffer))
-    (+doom-dashboard-reload t)))
+    (+dashboard-reload t)))
 
 ;;;###autoload
-(defun +doom-dashboard/forward-button (n)
+(defun +dashboard/forward-button (n)
   "Like `forward-button', but don't wrap."
   (interactive "p")
   (forward-button n nil)
-  (+doom-dashboard--help-echo))
+  (+dashboard--help-echo))
 
 ;;;###autoload
-(defun +doom-dashboard/backward-button (n)
+(defun +dashboard/backward-button (n)
   "Like `backward-button', but don't wrap."
   (interactive "p")
   (backward-button n nil)
-  (+doom-dashboard--help-echo))
+  (+dashboard--help-echo))

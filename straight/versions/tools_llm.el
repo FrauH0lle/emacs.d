f24bd66ccff141c6d8a996729c10cb87f19d6ded
FrauH0lle <roland.goers@posteo.de>
2026-03-23 07:32:36 +0100

    chore(llm): Bump packages
    * straight/versions/core.el: Update packages.
    
    * straight/versions/tools_llm.el: Ditto.

--- straight/versions/tools_llm.el
+++ straight/versions/tools_llm.el
@@ -1,5 +1,5 @@
 (("gptel-agent" . "33a391871596c997c3dbce7f9f14457a94dd6c3c")
- ("mevedel" . "ddbb441c5c309c6d2eb5753e51691f4f3f3aa9fe")
+ ("mevedel" . "b465ea1501068f63e0de484f91938662538b1379")
  ("macher" . "4fa8fbb6b250b207723d380931a463bcbc8da9ca")
  ("gptel-quick" . "018ff2be8f860a1e8fe3966eec418ad635620c38")
  ("gptel" . "ad619463d1a9cb466b8dd575cc905f2554886cd7")

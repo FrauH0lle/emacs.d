a53687a1bf1923678768b5d53967b7df17a4d1af
FrauH0lle <roland.goers@posteo.de>
2026-06-13 14:48:26 +0200

    chore(llm): bump packages
    * straight/versions/tools_llm.el: Bump `mevedel`.

--- straight/versions/tools_llm.el
+++ straight/versions/tools_llm.el
@@ -1,5 +1,5 @@
 (("gptel-agent" . "2853a579154cb4528082a372db79ecdec1eb17ad")
- ("mevedel" . "b465ea1501068f63e0de484f91938662538b1379")
+ ("mevedel" . "6a9ce39b2e37e5856d30145f8606bbfb3888e21e")
  ("macher" . "4fa8fbb6b250b207723d380931a463bcbc8da9ca")
  ("gptel-quick" . "36fe296e016449433fa1213f4b89cb8dc7d4db5e")
  ("gptel" . "df08c1a4d4c6e253870b8aa1634922b8f56224e7")
